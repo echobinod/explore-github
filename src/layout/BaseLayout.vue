de3afbcee94f98b4f00c53e98f646dbfafd5dfdc
Binod Shahi <binod@Binods-MacBook-Air.local>
2022-01-07 00:42:55 +0545

    BaseLayout setup

---
+++ src/layout/BaseLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+export default {
+  name: "BaseLayout",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style>
+</style>
