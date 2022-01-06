653a229411b6fa0cd99be8fd93853754949321e4
Binod Shahi <binod@Binods-MacBook-Air.local>
2022-01-07 02:29:36 +0545

    Repo search page with filter and pagination added

---
+++ src/components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card shadow-sm p-3 mb-5 bg-white repo__card">
+    <div class="card-body">
+      <h5 class="card-title">
+        <a
+          class="text-decoration-none text-dark"
+          @click="gotoRepoDetail(repo.id)"
+          >{{ repo.name }}</a
+        >
+      </h5>
+      <h6 class="card-subtitle text-muted mb-2">
+        <a href class="text-decoration-none text-muted">{{
+          repo.owner.login
+        }}</a>
+      </h6>
+      <p class="card-text">
+        {{ repo.description }}
+      </p>
+      <div class="d-flex mb-2">
+        <div class="me-3">
+          <a href="#" class="card-link text-decoration-none" title="Star">
+            <i class="fa fa-star-o" aria-hidden="true"></i>
+            {{ repo.stargazers_count }}</a
+          >
+        </div>
+        <div class="me-3">
+          <a href="#" class="card-link text-decoration-none" title="Fork">
+            <i class="panel-icon fa fa-code-fork"></i>
+            {{ repo.forks }}</a
+          >
+        </div>
+        <div class="me-3">
+          <a href="#" class="card-link text-decoration-none" title="Watcher">
+            <i class="fa fa-eye" aria-hidden="true"></i>
+            {{ repo.watchers }}
+          </a>
+        </div>
+      </div>
+      <span class="text-muted mb-2 timestamp">
+        Last update:
+        {{ new Date(repo.updated_at).toISOString().split("T")[0] }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["repo", "gotoRepoDetail"],
+};
+</script>
+
+<style>
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,106 +1,158 @@
 <template>
-  <div class="container pt-4">
+  <base-layout>
+    <div class="container-fluid">
       <div class="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
-        <div class="input-group">
-          <input
-            type="search"
-            placeholder="Search Repositories..."
-            aria-describedby="button-addon1"
-            class="form-control border-0 bg-light"
-          />
-          <div class="input-group-append">
-            <button
-              id="button-addon1"
-              type="submit"
-              class="btn btn-link text-primary"
-            >
-              <i class="fa fa-search"></i>
-            </button>
+        <form @submit.prevent="searchRepo">
+          <div class="input-group">
+            <input
+              type="search"
+              v-model="repoInput"
+              placeholder="Search Repositories..."
+              aria-describedby="button-addon1"
+              class="form-control border-0 bg-light"
+            />
+            <div class="input-group-append">
+              <button
+                id="button-addon1"
+                type="submit"
+                class="btn btn-link text-primary"
+              >
+                <i class="fa fa-search"></i>
+              </button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
-
       <div class="row justify-content-end mb-3">
-        <div class="col-1 text-end">
-          <select class="form-control">
-            <option value="ascendig">A-Z</option>
+        <div class="col-1">
+          <label for="sort">Sort</label>
+          <select class="form-select" @change="sortResult($event.target.value)">
+            <option></option>
+            <option value="desc">DESC</option>
+            <option value="asc">ASC</option>
+          </select>
+        </div>
+        <div class="col-1">
+          <label for="filter">Filter</label>
+          <select
+            class="form-select"
+            @change="filterPerPage($event.target.value)"
+          >
+            <option></option>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
           </select>
         </div>
       </div>
 
       <div class="row">
-        <div class="col-lg-4 mb-3">
-          <div class="card shadow-sm p-3 mb-5 bg-white repo__card">
-            <div class="card-body">
-              <h5 class="card-title">
-                <router-link :to="{ name: 'repoDetail' }" class="text-decoration-none text-dark">Repo Name</router-link>
-              </h5>
-              <h6 class="card-subtitle text-muted mb-2">
-                <a href class="text-decoration-none text-muted">Author Name</a>
-              </h6>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <div class="d-flex mb-2">
-                <div class="me-3">
-                  <a href="#" class="card-link text-decoration-none" title="Star">
-                    <i class="fa fa-star-o" aria-hidden="true"></i> 12</a
-                  >
-                </div>
-                <div class="me-3">
-                  <a href="#" class="card-link text-decoration-none" title="Fork">
-                    <i class="panel-icon fa fa-code-fork"></i> 12</a
-                  >
-                </div>
-                <div class="me-3">
-                  <a href="#" class="card-link text-decoration-none" title="Watcher">
-                    <i class="fa fa-eye" aria-hidden="true"></i> 12
-                  </a>
-                </div>
-              </div>
-              <span class="text-muted mb-2 timestamp">
-                Last Updated: 25 days ago
-              </span>
-            </div>
-          </div>
+        <div
+          v-for="repo in displayedRepos"
+          :key="repo.id"
+          class="col-lg-4 mb-3"
+        >
+          <card :repo="repo" :gotoRepoDetail="gotoRepoDetail"></card>
         </div>
       </div>
 
-      <nav aria-label="...">
+      <nav>
         <ul class="pagination justify-content-center">
-          <li class="page-item disabled">
-            <a class="page-link" href="#" tabindex="-1" aria-disabled="true"
-              >Previous</a
-            >
+          <li class="page-item" v-if="page != 1" @click="page--">
+            <a class="page-link"> Previous </a>
           </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item active" aria-current="page">
-            <a class="page-link" href="#">2</a>
+          <li
+            class="page-item"
+            :class="{ active: page == pageNumber }"
+            v-for="(pageNumber, index) in pages.slice(page - 1, page + 5)"
+            @click="page = pageNumber"
+            :key="index"
+          >
+            <a class="page-link">{{ pageNumber }}</a>
           </li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
           <li class="page-item">
-            <a class="page-link" href="#">Next</a>
+            <a class="page-link" @click="page++" v-if="page < pages.length"
+              >Next</a
+            >
           </li>
         </ul>
       </nav>
     </div>
+  </base-layout>
 </template>
 
 <script>
+import { RepoService } from "../services/repo.service";
+import BaseLayout from "../layout/BaseLayout.vue";
+import card from "@/components/Card.vue";
+
 export default {
-   name: 'HomePage',
-   mounted() {
-    
-   },
-   data() {
+  components: { BaseLayout, card },
+  name: "HomePage",
+  mounted() {
+    // this.repoInput = "vuejs";
+    // await this.searchRepo();
+  },
+  data() {
     return {
-     repos: [],
-    }
-   }
-}
+      repoInput: "",
+      repos: [],
+      page: 1,
+      perPage: 6,
+      pages: [],
+    };
+  },
+  methods: {
+    async searchRepo() {
+      this.repos = [];
+      const data = await RepoService.getRepo(this.repoInput);
+      this.repos = data.items;
+    },
+    setPages() {
+      let numberOfPages = Math.ceil(this.repos.length / this.perPage);
+      for (let index = 1; index <= numberOfPages; index++) {
+        this.pages.push(index);
+      }
+    },
+    paginate(repos) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = page * perPage - perPage;
+      let to = page * perPage;
+      return repos.slice(from, to);
+    },
+    gotoRepoDetail(id) {
+      let data = this.repos.find((repo) => repo.id === id);
+      this.$router.push({
+        // name: "repoDetail",
+        params: { data },
+        path: `/repo/${data.owner.login}/${data.name}`,
+      });
+    },
+    async sortResult(sortBy) {
+      this.repos = [];
+      this.pages = [];
+      const data = await RepoService.sortRepo(sortBy, this.repoInput);
+      this.repos = data.items;
+    },
+    filterPerPage(num) {
+      this.perPage = num;
+      this.pages = [];
+      this.setPages();
+    },
+  },
+  computed: {
+    displayedRepos() {
+      return this.paginate(this.repos);
+    },
+  },
+  watch: {
+    repos() {
+      this.setPages();
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
